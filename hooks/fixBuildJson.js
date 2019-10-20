d54c71a2eaacba86d450f876bea5a4487db627fc
Carlos Sotelo <carlos.sotelo7@gmail.com>
2019-10-20 01:23:34 +0100

    fix: change build.json to automatic provisioning

---
+++ hooks/fixBuildJson.js
@@ -0,0 +1,27 @@
+// @ts-check
+var path = require('path');
+var Q = require('q');
+
+module.exports = function (context) {
+  var deferral = new Q.defer();
+  var fs = require('fs')
+  var file = path.join(context.opts.projectRoot, "build.json")
+  console.log('Fixing build.json');
+
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('build.json found, fixing it')
+    var fileContent = JSON.parse(data);
+    delete fileContent.ios.debug.provisioningProfile;
+    delete fileContent.ios.release.provisioningProfile;
+
+    fs.writeFile(file,JSON.stringify(fileContent), 'utf8', function (err) {
+      if (err) return console.log(err);
+      console.log('build.json changed successfully')
+      deferral.resolve();
+    });
+  });
+  return deferral.promise;
+};
